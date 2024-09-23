0f2d402ababdd90a8d7eca624a6096fd008d029b
노광우 <ngw960@gmail.com>
2024-09-23 20:11:47 +0900

    오류 수정

--- Corp_project/asdf/urls.py
+++ Corp_project/asdf/urls.py
@@ -1,30 +1,45 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
+    path('', views.Index_Page, name = 'Index_Page'),
     
-    path('', views.Main_Page, name = '메인 페이지'),
+    path('signup/', views.Signup_Page, name = 'Signup_Page'),
 
-    path('signup/', views.Signup_Page, name = '회원가입 페이지'),
+    path('login/', views.Login_Page, name = 'Login_Page'),
 
-    path('signup_action/', views.Signup_Action, name = '회원 가입하기'),
+    path('signup_action/', views.Signup_Action, name = 'Signup_Action'),
 
-    path('login/', views.Login_Page, name = '로그인 페이지'),
+    path('login_action/', views.Login_Action, name = 'Login_Action'),
 
-    path('login_action/', views.Login_Action, name = '로그인 하기'),
+    path('logout/', views.Logout, name = 'Logout'),
 
-    path('logout/', views.Logout, name = '로그아웃'),
+    path('board/', views.Board_Page, name = 'Board_Page'),
 
-    path('board/', views.Board_Page, name = '글쓰기 게시판 페이지'),
+    path('post_writing/', views.Post_Writing_Page, name = 'Post_Writing_Page'),
 
-    path('post_writing/', views.Post_Writing_Page, name = '글 작성 페이지'),
+    path('post_writing_action/', views.Post_Writing_Action, name = 'Post_Writing_Action'),
 
-    path('post_writing_action/', views.Post_Writing_Action, name = '글 작성하기'),
+    path('post_detail/<int:post_id>/', views.Post_Detail_Page, name = 'Post_Detail_Page'),
 
-    path('post_detail/<int:post_id>/', views.Post_Detail_Page, name = '글 상세보기 페이지'),
+    path('myaccount/', views.Myaccount_Page, name = 'Myaccount_Page'),
 
+    path('edit_info', views.Edit_Info, name = 'Edit_Info'),
 
-    
+    path('user_detail/<int:connected_user_id>/', views.User_Detail_Page, name = 'User_Detail_Page'),
+
+    path('delete_post/<int:post_id>', views.Delete_Post, name = 'Delete_Post'),
+
+    path('message_box/', views.Message_Box_Page, name = 'Message_Box_Page'),
+
+    path('message_writing/', views.Message_Writing_Page, name = 'Message_Writing_Page'),
+
+    path('message_writing_action/', views.Message_Writing_Action, name = 'Message_Writing_Action'),
+
+    path('message_detail/<int:message_id>/', views.Message_Detail_Page, name = 'Message_Detail_Page'),
+
+    path('message_reply/<int:connected_user_id>/', views.Message_Reply_Page, name = 'Message_Reply_Page'),
+
+    path('message_delete/<int:message_id>/', views.Message_Delete, name = 'Message_Delete'),
+
 ]
