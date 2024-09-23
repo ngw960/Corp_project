62e9a7626c049486a9ec13354cb9d24e24694b61
노광우 <ngw960@gmail.com>
2024-09-23 18:49:32 +0900

    회원가입 기능
    - 회원가입 페이지 템플릿 추가
    - 회원가입 URL 패턴 구성
    - 아이디, 비밀번호, 이메일 등을 입력하여 회원가입 가능

--- Corp_project/asdf/views.py
+++ Corp_project/asdf/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Users
 
 
 
 def Main_Page(request):
     return render(request, "main.html")
 
+
+def Signup_Page(request):
+    return render(request, "signup.html")
+
+
+def Signup_Action(request):
+    # 입력받은 값 정의
+    var_user_id = request.POST.get('input_user_id')
+    var_password = request.POST.get('input_password')
+    var_name = request.POST.get('input_name')
+    var_email = request.POST.get('input_email')
+
+    # 빈 필드가 없는지 검증
+    if var_user_id and var_password and var_name and var_email:
+
+        # 입력받은 아이디, 이메일이 db에 존재하는지 검증
+        if Users.objects.filter(user_id = var_user_id).exists():
+            return redirect('Signup_Page')
+    
+        if Users.objects.filter(email = var_email).exists():
+            return redirect('Signup_Page')
+        
+        # db에 입력받은 유저 정보 저장
+        Users.objects.create(
+            user_id = var_user_id,
+            password = var_password,
+            name = var_name,
+            email = var_email
+        )
+        
+        return render(request, "login.html")
+
+    else:
+        return redirect('Signup_Page')
 
