cc860ec9f324e54839d329684af8462b6f153ddd
노광우 <ngw960@gmail.com>
2024-09-23 19:34:24 +0900

    글쓰기 기능,글쓰기 게시판
    - 글쓰기 페이지 템플릿 추가
    - 글쓰기 URL 패턴 구성
    - 글 제목과 내용을 입력해서 글 등록 가능

--- Corp_project/asdf/urls.py
+++ Corp_project/asdf/urls.py
@@ -17,5 +17,14 @@ urlpatterns = [
 
     path('logout/', views.Logout, name = '로그아웃'),
 
+    path('board/', views.Board_Page, name = '글쓰기 게시판 페이지'),
+
+    path('post_writing/', views.Post_Writing_Page, name = '글 작성 페이지'),
+
+    path('post_writing_action/', views.Post_Writing_Action, name = '글 작성하기'),
+
+    path('post_detail/<int:post_id>/', views.Post_Detail_Page, name = '글 상세보기 페이지'),
+
+
     
 ]
